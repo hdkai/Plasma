f35b4d9db22f699a89d7facf03f16fabe020e87c
Yusuf Olokoba <lanre@krikey.com>
2020-08-13 16:27:29 -0400

    Add Gaussian layers

--- plasma/nn/gaussian.py
+++ plasma/nn/gaussian.py
@@ -6,25 +6,30 @@
 from torch import Tensor
 from torch.nn import Module
 
-class Gaussian2D (Module): # INCOMPLETE
+from ..sampling import gaussian_blur_2d, gaussian_blur_3d
+
+class Gaussian2D (Module):
     """
     2D Gaussian smoothing layer.
     """
 
-    def __init__ (self):
+    def __init__ (self, size):
         super(Gaussian2D, self).__init__()
+        self.__size = size if isinstance(size, tuple) else (size, size)
 
     def forward (self, input):
-        pass
+        result = gaussian_blur_2d(input, self.__size)
+        return result
 
-
-class Gaussian3D (Module): # INCOMPLETE
+class Gaussian3D (Module):
     """
     3D Gaussian smoothing layer.
     """
 
-    def __init__ (self):
+    def __init__ (self, size):
         super(Gaussian3D, self).__init__()
+        self.__size = size if isinstance(size, tuple) else (size, size, size)
 
     def forward (self, input):
-        pass
+        result = gaussian_blur_3d(input, self.__size)
+        return result
