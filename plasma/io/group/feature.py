737856f38c1977deee4fdb3d7505e8c740db4582
Yusuf Olokoba <lanre@krikey.com>
2020-09-22 19:20:18 -0400

    Create exposure grouping module

---
+++ plasma/io/group/feature.py
@@ -0,0 +1,19 @@
+# 
+#   Plasma
+#   Copyright (c) 2020 Homedeck, LLC.
+#
+
+from typing import List
+
+def group_exposures_by_features (exposure_paths: List[str], workers=4) -> List[List[str]]: # INCOMPLETE
+    """
+    Group a set of exposures by their visual features.
+
+    Parameters:
+        exposure_paths (list): Paths to exposures to group.
+        workers (int): Number of workers for IO.
+    
+    Returns:
+        list: Groups of exposure paths.
+    """
+    pass
---
+++ plasma/io/group/timestamp.py
@@ -0,0 +1,35 @@
+# 
+#   Plasma
+#   Copyright (c) 2020 Homedeck, LLC.
+#
+
+from numpy import array
+from sklearn.cluster import DBSCAN
+from typing import List
+
+from .common import exposure_timestamp
+
+def group_exposures_by_timestamp (exposure_paths: List[str], max_delta=8.) -> List[List[str]]:
+    """
+    Group a set of exposures by their visual features.
+
+    Parameters:
+        exposure_paths (list): Paths to exposures to group.
+        max_delta (float): Maximum exposure time difference for separating exposures, in seconds.
+    
+    Returns:
+        list: Groups of exposure paths.
+    """
+    # Check
+    if not exposure_paths:
+        return []
+    # Get timestamps
+    timestamps = [exposure_timestamp(path) for path in exposure_paths]
+    timestamps = [time if time >= 0 else -(i + 1) * max_delta * 10 for i, time in enumerate(timestamps)]
+    # Cluster
+    timestamps = array([timestamps]).T
+    timestamps -= timestamps.min()
+    dbscan = DBSCAN(eps=max_delta, min_samples=1, p=1)
+    labels = dbscan.fit_predict(timestamps)
+    groups = [[path for idx, path in enumerate(exposure_paths) if labels[idx] == i] for i in range(labels.max() + 1)]
+    return groups
