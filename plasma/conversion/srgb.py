ccb682b60e81c35fe24493d3e3ca81a0494f0918
Yusuf Olokoba <lanre@krikey.com>
2020-10-05 21:39:13 -0400

    Implementing Lab and XYZ

---
+++ plasma/conversion/srgb.py
@@ -0,0 +1,18 @@
+# 
+#   Plasma
+#   Copyright (c) 2020 Homedeck, LLC.
+#
+
+from torch import where, Tensor
+
+def srgb_to_linear (input: Tensor) -> Tensor:
+    """
+
+    """
+    pass
+
+def linear_to_srgb (input: Tensor) -> Tensor:
+    """
+
+    """
+    pass
