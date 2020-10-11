23ebcd6735d021e1beb1fab1c9fa702c4f44f918
Yusuf Olokoba <lanre@krikey.com>
2020-10-11 19:18:48 -0400

    Disable threading

--- plasma/io/group/group.py
+++ plasma/io/group/group.py
@@ -27,23 +27,42 @@ def group_exposures (exposure_paths: List[str], similarity_fn: Callable[[Image.I
     # Trivial case
     if len(exposure_paths) == 1:
         return [exposure_paths]
-    # Load all exposures into memory
-    with ThreadPoolExecutor(max_workers=workers) as executor:
-        # Sort by timestamp
-        exposures = executor.map(lambda path: Image.open(path), exposure_paths)
-        exposures_with_paths = zip(exposure_paths, exposures)
-        exposures_with_paths = sorted(exposures_with_paths, key=lambda pair: exposure_timestamp(pair[1]))
-        exposure_paths, exposures = list(zip(*exposures_with_paths))
-        # Compute pairwise similarity
-        pairwise_similarities = executor.map(lambda pair: similarity_fn(*pair), [(exposures[i], exposures[i+1]) for i in range(len(exposures) - 1)])
-        pairwise_similarities = list(pairwise_similarities) # So we can see it when debugging
+    
+    # # Load all exposures into memory
+    # with ThreadPoolExecutor(max_workers=workers) as executor:
+    #     # Sort by timestamp
+    #     exposures = executor.map(lambda path: Image.open(path), exposure_paths)
+    #     exposures_with_paths = zip(exposure_paths, exposures)
+    #     exposures_with_paths = sorted(exposures_with_paths, key=lambda pair: exposure_timestamp(pair[1]))
+    #     exposure_paths, exposures = list(zip(*exposures_with_paths))
+    #     # Compute pairwise similarity
+    #     pairwise_similarities = executor.map(lambda pair: similarity_fn(*pair), [(exposures[i], exposures[i+1]) for i in range(len(exposures) - 1)])
+    #     pairwise_similarities = list(pairwise_similarities) # So we can see it when debugging
+    # # Group
+    # groups = []
+    # current_group = [exposure_paths[0]]
+    # for i, similar in enumerate(pairwise_similarities):
+    #     if not similar:
+    #         groups.append(current_group)
+    #         current_group = []
+    #     current_group.append(exposure_paths[i+1])
+    # groups.append(current_group)
+
+    # Load exposures
+    exposures = [Image.open(path) for path in exposure_paths]
+    exposures_with_paths = zip(exposure_paths, exposures)
+    exposures_with_paths = sorted(exposures_with_paths, key=lambda pair: exposure_timestamp(pair[1]))
     # Group
+    current_path, current_exposure = exposures_with_paths.pop(0)
     groups = []
-    current_group = [exposure_paths[0]]
-    for i, similar in enumerate(pairwise_similarities):
-        if not similar:
+    current_group = [current_path]
+    while exposures_with_paths:
+        path, exposure = exposures_with_paths.pop(0)
+        if not similarity_fn(current_exposure, exposure):
             groups.append(current_group)
             current_group = []
-        current_group.append(exposure_paths[i+1])
+        current_group.append(path)
+        current_path, current_exposure = path, exposure
     groups.append(current_group)
+
     return groups
